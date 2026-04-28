95c9c73ab702fab51942e78bad933e6bc6628569
saeed-2008-ship-it <saeed-_2008@hotmail.com>
2026-04-28 17:30:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
