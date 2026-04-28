7fca254cb8731fc3957d6523a95cbe7bbb7ed80f
saeed-2008-ship-it <saeed-_2008@hotmail.com>
2026-04-28 17:29:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 const SECRET = process.env.JWT_SECRET || 'secret';
 
