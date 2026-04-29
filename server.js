6d2a9fdb2188fdb127f5aaf9a507e2248c724994
saeed-2008-ship-it <saeed-_2008@hotmail.com>
2026-04-29 12:18:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const valid = password === user.password;
 
 // AUTH middleware
 function auth(req,res,next){
-  const token = req.headers.authorization;
+const token = req.headers.authorization?.replace("Bearer ", "");
   if(!token) return res.sendStatus(401);
   jwt.verify(token, SECRET,(err,user)=>{
     if(err) return res.sendStatus(403);
