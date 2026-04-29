3326db7a4ee5a0b84b70f3977df4f1bd4cf047ad
saeed-2008-ship-it <saeed-_2008@hotmail.com>
2026-04-29 11:22:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const cors = require('cors');
 const db = require('./db');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
